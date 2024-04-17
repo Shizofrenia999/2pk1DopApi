d452ba165cdd17fd4d4d76a7489a8fca1b356135
Снежок <144549734+Shizofrenia999@users.noreply.github.com>
2024-04-17 07:16:22 +0300

    Add files via upload

---
+++ demo/src/main/java/com/simpleRestApi/demo/controllers/HomeController.java
@@ -0,0 +1,12 @@
+package com.simpleRestApi.demo.controllers;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+@Controller@RequestMapping("/rest/home") class HomeController {
+    @ResponseBody
+    @RequestMapping(value = "", method=RequestMethod.GET)
+    public String hello(){
+        return "ответ с endpoint rest/home";
+    }
+}
